ebbbc815079671b1a3a0a4c5ec03d58562f3c77e
ahernd2 <david.ahern26@gmail.com>
2017-07-11 21:50:20 +0100

    Fixing lint

--- src/app/release-toggles/release-toggles.reducer.spec.ts
+++ src/app/release-toggles/release-toggles.reducer.spec.ts
@@ -6,34 +6,55 @@ describe('Release Toggles Reducer', () => {
 
   it('should start fetching release toggles', () => {
     const action = {type: actionTypes.FETCH_RELEASE_TOGGLES};
-    expect(releaseTogglesReducer(releaseToggleState, action)).toEqual({...releaseToggleState, isFetchingReleaseToggles: true});
+    expect(releaseTogglesReducer(releaseToggleState, action)).toEqual({
+      ...releaseToggleState,
+      isFetchingReleaseToggles: true
+    });
   });
 
   it('should finish fetching release toggles successfully', () => {
     const action = {type: actionTypes.UPDATE_RELEASE_TOGGLES, releaseToggles: [{}]};
-    expect(releaseTogglesReducer(releaseToggleState, action)).toEqual({...releaseToggleState, releaseToggles: [{}], isFetchingReleaseToggles: false});
+    expect(releaseTogglesReducer(releaseToggleState, action)).toEqual({
+      ...releaseToggleState,
+      releaseToggles: [{}],
+      isFetchingReleaseToggles: false
+    });
   });
 
   it('should indicate fetching release toggles failed', () => {
     const action = {type: actionTypes.FETCH_RELEASE_TOGGLES_FAILED};
-    expect(releaseTogglesReducer(releaseToggleState, action)).toEqual({...releaseToggleState, isFetchingReleaseToggles: false});
+    expect(releaseTogglesReducer(releaseToggleState, action)).toEqual({
+      ...releaseToggleState,
+      isFetchingReleaseToggles: false
+    });
   });
 
   it('should show edit toggle modal', () => {
     const action = {type: actionTypes.SHOW_EDIT_TOGGLE_MODAL, releaseToggle: {}};
-    expect(releaseTogglesReducer(releaseToggleState, action)).toEqual({...releaseToggleState, editReleaseToggle: {}, showEditToggleModal: true});
+    expect(releaseTogglesReducer(releaseToggleState, action)).toEqual({
+      ...releaseToggleState,
+      editReleaseToggle: {},
+      showEditToggleModal: true
+    });
   });
 
   it('should hide edit toggle modal', () => {
     const state = {...releaseToggleState, showEditToggleModal: true};
     const action = {type: actionTypes.HIDE_EDIT_TOGGLE_MODAL};
-    expect(releaseTogglesReducer(state, action)).toEqual({...releaseToggleState, showEditToggleModal: false, editReleaseToggle: {}});
+    expect(releaseTogglesReducer(state, action)).toEqual({
+      ...releaseToggleState,
+      showEditToggleModal: false,
+      editReleaseToggle: {}
+    });
   });
 
   it('should indicate toggle edit succeeded', () => {
     const state = {...releaseToggleState, releaseToggles: [{id: '1'}, {id: '2'}]} as ReleaseToggleState;
     const action = {type: actionTypes.REQUEST_TOGGLE_EDIT_SUCCESS, releaseToggle: {id: '2', name: 'abc'}};
-    expect(releaseTogglesReducer(state, action)).toEqual({...releaseToggleState, releaseToggles: [{ id: '1'}, {id: '2', name: 'abc'}]});
+    expect(releaseTogglesReducer(state, action)).toEqual({
+      ...releaseToggleState,
+      releaseToggles: [{id: '1'}, {id: '2', name: 'abc'}]
+    });
   });
 
   it('should return initial state for unknown actions', () => {
